a5596174990c222c83cda9a597030196fb3f2785
Areeb <areebaizam@gmail.com>
2024-05-02 00:32:59 -0700

    Refactor Endpoint

---
+++ Endpoints/PrayerEndpoints.cs
@@ -0,0 +1,17 @@
+﻿namespace MAPI.Endpoints
+{
+    public static class PrayerEndpoints
+    {
+        public static void MapPrayerEndpoints(this WebApplication app)
+        {
+            //Base URL
+            var group = app.MapGroup("api/prayer");
+            group.MapGet("", GetPrayers).WithName("GetPrayerTimes").WithOpenApi();
+        }
+
+        public static string GetPrayers()
+        {
+            return "Hello, World!";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,7 +5,11 @@
 `dotnet dev-certs https --trust`
 
 ## Run an app with https profile in launchSettings
+## OR Change the order of profile
 `dotnet run --launch-profile "https"`
 
 ## Test app using swagger url
 `https://localhost:7283/swagger/index.html`
+
+## Build an app after changes
+`dotnet build`
